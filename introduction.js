36b3a2ec30d0357a935e0330900f5fe71d8c133c
unknown <ee1206@hotmail.com>
2015-06-25 12:01:59 -0500

    Ejemplos de javascripting

---
+++ introduction.js
@@ -0,0 +1,3 @@
+//Para crear un archivo desde el cmd el comando es:
+//type NUL > introduction.js
+console.log('hello');
---
+++ number-to-string.js
@@ -0,0 +1,6 @@
+var n = "128";
+var m ="133";
+var b ="300";
+console.log(n.toString());
+console.log(m.toString());
+console.log(b.toString());
